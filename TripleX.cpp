bb5894a8f9cdaca3bbe98721f7c415cb095dcbe9
Pawel <pawelszalawinski@gmail.com>
2022-02-28 22:53:48 +0100

    game finished

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,21 +1,103 @@
 #include <iostream>
 
+void PrintIntroduction(int LevelDifficulty)
+{
+    std::cout << "\n\nHello! You are going to play the most annoying game in a minute...\n";
+    std::cout << "Enter corect code to continue...\n";
+    std::cout << "LevelDifficulty is - " << LevelDifficulty;
+    std::cout << "\n There are 3 numbers in the code\n";
+}
+bool PlayGame(int LevelDifficulty)
+{
+
+    PrintIntroduction(LevelDifficulty);
+
+    int LevelMultiplier = 3 * LevelDifficulty;
+
+    const int GuessA = 1 + rand() % LevelMultiplier;
+    const int GuessB = 1 + rand() % LevelMultiplier;
+    const int GuessC = 1 + rand() % LevelMultiplier;
+
+    std::cout << GuessA << " " << GuessB << " " << GuessC << std::endl;
+
+    int GuessSum, GuessProduct;
+
+    // std::cin >> GuessA >> GuessB >> GuessC;
+
+    const int ExpectedSum = GuessA + GuessB + GuessC;
+    const int ExpectedProduct = GuessA * GuessB * GuessC;
+
+    std::cout << "What is the sum?\n";
+    std::cin >> GuessSum;
+    std::cout << "What is the multiply?\n";
+    std::cin >> GuessProduct;
+
+    if (GuessSum == ExpectedSum && GuessProduct == ExpectedProduct)
+    {
+        std::cout << "You WON!";
+        return true;
+    }
+    else
+    {
+        std::cout << "\nYou LOST!\n";
+        std::cout << "sum is " << ExpectedSum;
+        std::cout << "multiply is " << ExpectedProduct;
+
+        return false;
+    }
+}
+/*
+     {
+         bool status = true;
+
+         if (GuessSum == ExpectedSum)
+         {
+             std::cout << "Sum is correct!\n";
+         }
+         else
+         {
+             std::cout << "\nSum is NOT correct!";
+             std::cout << "\n+++ The numbers sum to: " << ExpectedSum;
+             status = false;
+         }
+
+         if (GuessProduct == ExpectedProduct)
+         {
+             std::cout << "\nMultiply is correct!";
+         }
+         else
+         {
+             std::cout << "\nMultiply is NOT correct!\n";
+             std::cout << "+++ The numbers multiplies to: " << ExpectedProduct;
+             status = false;
+         }
+
+         if (!status)
+         {
+             std::cout << "\nYou LOST!\n";
+             isNotEnd = true;
+         }
+     }
+     */
+
 int main()
 {
-    std::cout << "Hello! You are going to play the most annoying game in a minute. But firstly please write your name...";
-    std::cout << std::endl;
-    std::cout << "Enter corect code to continue...";
 
-    const int a = 4;
-    const int b = 4;
-    const int c = 4;
+    int LevelDifficulty = 1;
+    const int MaxLevel = 4;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    while (LevelDifficulty <= MaxLevel)
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear();
+        std::cin.ignore();
 
-    std::cout << std::endl;
-    std::cout << product << std::endl;
-    std::cout << sum << std::endl;
-
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+    }
+    std::cout << "\nCongratulations you have reached maximum Level!";
+    std::cout << "\nTHE END!";
     return 0;
 }
